1b438729439072552b1fb7f082a00f87b6963b83
FranckLejzerowicz <flejzerowicz@ucsd.edu>
2020-08-08 14:36:52 -0700

    st

--- Xsourcetracking/feast.py
+++ Xsourcetracking/feast.py
@@ -42,10 +42,6 @@ def run_feast(
         diff_sources: bool,
         p_times: int) -> str:
 
-    # get the sink samples broken down into sublists based on p_sink
-    # (all sink samples must be vs. sources but not necessarily at once)
-    # sink_samples_chunks = get_sink_samples_chunks(samples, sink, p_size, p_chunks)
-
     params = get_params(p_iterations_burnins, p_rarefaction, diff_sources)
     r_script = '%s/run_feast.R' % o_dir_path_meth
     with open(r_script, 'w') as r_o:
@@ -87,21 +83,5 @@ def run_feast(
                 r_o.write('FEAST(C=feat, metadata=meta, dir_path="%s", outfile="out.r%s"%s)\n' % (
                     o_dir_path_meth_t, r, params))
 
-
-            # for cdx, chunk in enumerate(sink_samples_chunks):
-            #     n_sources = get_chunk_nsources(chunk, sources, counts)
-            #     r_meta = get_timechunk_meta(chunk, sink, sources, samples, n_sources, 'feast')
-            #     sams = '","'.join(r_meta['SampleID'].tolist())
-            #     r_o.write('samples <- c("%s")\n' % sams)
-            #     r_o.write('meta <- data.frame(Env=c("%s"), SourceSink=c("%s"), id=c(%s))\n' % (
-            #         '","'.join(r_meta['Env'].tolist()),
-            #         '","'.join(r_meta['SourceSink'].tolist()),
-            #         ','.join(map(str, r_meta['id'].tolist())),
-            #     ))
-            #     r_o.write('rownames(meta) <- samples\n')
-            #     r_o.write('feat <- feats_full[samples,]\n' % ())
-            #     r_o.write('feat <- feats_full[,colSums(feats_full)>0]\n' % ())
-            #     r_o.write('o.t%s.c%s <- FEAST(C=feat, metadata=meta, dir_path="%s", outfile="o.t%s.c%s"%s)\n' % (
-            #         t, cdx, o_dir_path_meth, t, cdx, params))
     cmd = 'conda activate feast\nR -f %s --vanilla' % r_script
     return cmd
