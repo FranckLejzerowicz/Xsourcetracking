b5d11301fcccb08ab6be86f7dc9c35ecaa2633da
FranckLejzerowicz <franck.lejzerowicz@gmail.com>
2026-06-12 19:14:07 +0200

    rm dir

--- Xsourcetracking/tests/example/table_abund500/var-A/sourcetracker/cmd_sourcetracker.sh
+++
@@ -1,3 +0,0 @@
-source activate st2
-sourcetracker2 -i /Users/flejzerowicz/programs/Xsourcetracking/Xsourcetracking/tests/example/table_abund500/var-A/tab.biom -m /Users/flejzerowicz/programs/Xsourcetracking/Xsourcetracking/tests/example/table_abund500/var-A/sourcetracker/t0/map.loo.tsv --loo --jobs 10 -o /Users/flejzerowicz/programs/Xsourcetracking/Xsourcetracking/tests/example/table_abund500/var-A/sourcetracker/t0/loo
-
--- Xsourcetracking/tests/nonstool745s/env_material-feces/feast/cmd_feast.sh
+++
@@ -1,2 +0,0 @@
-source activate feast
-R -f /Users/flejzerowicz/programs/Xsourcetracking/Xsourcetracking/tests/nonstool745s/env_material-feces/feast/run_feast.R --vanilla
